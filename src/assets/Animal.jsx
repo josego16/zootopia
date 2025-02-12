32fdc48176372c2be9e4e209d2e297a06e752fc6
josego <josemagomez53@gmail.com>
2025-02-12 18:13:45 +0100

    listado de animales completado

--- src/assets/Animal.jsx
+++ src/assets/Animal.jsx
@@ -7,30 +7,33 @@ const Animal = ({countries, locations, animal, onDeleteAnimal}) => {
             ? animal.description
             : animal.description.substring(0, 100) + '...';
     };
+
     const getCountries = () => {
-        const cy = countries.find((country) => country.id === animal.id);
-        if (cy) {
-            return cy.name;
+        const coun = countries.filter((country) => animal.countries.includes(country.id));
+        if (coun) {
+            return coun.map(country => country.name).join(', ');
         } else {
             return "No clasificado";
         }
-    }
+    };
+
     const getLocation = () => {
-        const loc = locations.find((location) => location.id === animal.id);
-        if (loc) {
-            return loc.name;
+        const local = locations.filter((location) => animal.locations.includes(location.id));
+        if (local) {
+            return local.map(location => location.name).join(', ');
         } else {
             return "No clasificado";
         }
-    }
+    };
+
     return (
         <div className="animal-top">
             <h1>{animal.name}</h1>
             <img src={animal.image_url} alt={animal.name}/>
             <div className="animal-bottom">
                 <p><b>Pais: </b>{getCountries()}</p>
-                <p><b>Localizacion: </b>{getLocation()}</p>
-                <p><b>Descripcion: </b>{showDescription()}</p>
+                <p><b>Localización: </b>{getLocation()}</p>
+                <p><b>Descripción: </b>{showDescription()}</p>
             </div>
             <Button animal={animal} onDeleteAnimal={onDeleteAnimal}/>
         </div>
